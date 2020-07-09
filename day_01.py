b066577045f5bc293d636c6c0f566ef018dd1885
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-07-09 20:14:51 +0500

    update

--- day_01.py
+++ day_01.py
@@ -20,13 +20,13 @@
 # print(fact(x))
 
 # Q:3. with a given integral number n, write a program to generate a dictionary
-#  that contains (i, i*i) such that is an integral number between 1 and n (both included). 
-#  and then the program should print the dictionary.
+ that contains (i, i*i) such that is an integral number between 1 and n (both included). 
+ and then the program should print the dictionary.
 # ........
-n=int(input())
-d=dict()
-8
-for i in range(1,n+1):
-    # indexing 8
-    d[i] = i*i
-print(d)
+# n=int(input())
+# d=dict()
+# 8
+# for i in range(1,n+1):
+#     # indexing 8
+#     d[i] = i*i
+# print(d)
