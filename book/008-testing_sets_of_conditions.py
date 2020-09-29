59aa524adc25ee8e535cddc58a7b704403cdcd3d
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-29 12:50:59 +0500

    008-testing_sets_of_conditions.py file

---
+++ book/008-testing_sets_of_conditions.py
@@ -0,0 +1,26 @@
+# Testing sets of conditions
+
+# if any one condition doesn't met program not execute
+# weight = 350
+# time = 7
+
+# if weight > 300 and time < 6:
+#     print("Try to recruit hum..")
+
+
+# if any one condition doesn't met program not execute
+# weight = 35
+# time = 4
+# age = 18
+# height = 65
+# if weight > 30 and time < 6 and age > 17 and height < 72:
+#     print("Try To recruit him.. ")
+
+
+# if Any one conditio met program to be executed 
+SAT = 300
+avg = 200
+GPA = 2.3
+parents = "none"
+if SAT > avg or GPA > 2.5 or parents == "alum":
+    print("Welcome to Leeds College.")
