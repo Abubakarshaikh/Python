09344e3740b87ea7458558bc9c142467ffc7beed
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-29 12:52:17 +0500

    010-lists.py file

---
+++ book/010-lists.py
@@ -0,0 +1,50 @@
+# Objective: list
+
+# 1. Adding and changing elements
+# 2. Taking slices out of them
+# 3. Deleting and removing elements
+# 4. popping elements
+
+
+cities = ["Karachi", "Hyderaad", "Lahore", "Islamabad","Quetta","Multan"]
+
+# Taking slices out of them
+
+# smaller_list_cities = cities[2:5]
+# print(smaller_cities)
+
+# smaller_list_cities = cities[:5]
+# print(smaller_list_cities)
+
+# smaller_list_cities = cities[5:]
+# print(smaller_list_cities)
+
+
+
+# del method
+
+# del cities[0]
+# print(cities[0])
+
+# remove method
+
+# cities.remove("Hydeabad")
+# print(cities[1])
+
+# pop method
+
+# cities.pop()
+# print(cities)
+
+# cities.pop(3)
+# print(cities[4])
+# print(cities)
+
+# save = cities.pop()
+# print(sv)
+
+# cities.append(cities.pop(0)) 
+# print(cities)
+
+# cities.insert(1, cities.pop(0))
+# print(cities)
